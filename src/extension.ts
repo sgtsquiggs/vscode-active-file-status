f48fa0e075e6426071dfb9abbcaff51688858f22
Matthew Crenshaw <mcrenshaw@clearstreet.io>
2021-05-26 13:24:34 -0400

    fix outdated dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,79 +1,75 @@
 'use strict';
 
-import {Disposable, ExtensionContext, StatusBarItem, StatusBarAlignment, window, workspace} from 'vscode';
+import { Disposable, ExtensionContext, StatusBarItem, StatusBarAlignment, window, workspace } from 'vscode';
 import * as path from 'path';
 
 export function activate(context: ExtensionContext) {
 
-    let activeFile = new ActiveFile();
-    let controller = new ActiveFileController(activeFile);
+	const activeFile = new ActiveFile();
+	const controller = new ActiveFileController(activeFile);
 
-    context.subscriptions.push(controller);
-    context.subscriptions.push(activeFile);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+	context.subscriptions.push(controller);
+	context.subscriptions.push(activeFile);
 }
 
 class ActiveFile {
 
-    private _statusBarItem: StatusBarItem;
+	private _statusBarItem: StatusBarItem;
 
-    public updateActiveFile() {
+	public updateActiveFile() {
 
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-            this._statusBarItem.tooltip = 'Copy to Clipboard';
-            this._statusBarItem.command = 'workbench.action.files.copyPathOfActiveFile';
-        }
+		if (!this._statusBarItem) {
+			this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+			this._statusBarItem.tooltip = 'Copy to Clipboard';
+			this._statusBarItem.command = 'workbench.action.files.copyPathOfActiveFile';
+		}
 
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
+		const editor = window.activeTextEditor;
+		if (!editor) {
+			this._statusBarItem.hide();
+			return;
+		}
 
-        var filePath = editor.document.fileName;
-        if (!path.isAbsolute(filePath)) {
-            // Active editor is probably OUTPUT or DEBUG CONSOLE, so ignore
-            return;
-        }
-        if (filePath.indexOf(workspace.rootPath) == 0) {
-            filePath = path.relative(workspace.rootPath, filePath);
-        }
+		let filePath = editor.document.fileName;
+		if (!path.isAbsolute(filePath)) {
+			// Active editor is probably OUTPUT or DEBUG CONSOLE, so ignore
+			return;
+		}
+		if (filePath.indexOf(workspace.rootPath) == 0) {
+			filePath = path.relative(workspace.rootPath, filePath);
+		}
 
-        this._statusBarItem.text = filePath;
-        this._statusBarItem.show();
-    }
+		this._statusBarItem.text = filePath;
+		this._statusBarItem.show();
+	}
 
-    dispose() {
-        this._statusBarItem.dispose();
-    }
+	dispose() {
+		this._statusBarItem.dispose();
+	}
 }
 
 class ActiveFileController {
 
-    private _activeFile: ActiveFile;
-    private _disposable: Disposable;
+	private _activeFile: ActiveFile;
+	private _disposable: Disposable;
 
-    constructor(activeFile: ActiveFile) {
-        this._activeFile = activeFile;
-        this._activeFile.updateActiveFile();
+	constructor(activeFile: ActiveFile) {
+		this._activeFile = activeFile;
+		this._activeFile.updateActiveFile();
 
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+		const subscriptions: Disposable[] = [];
+		window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
 
-        this._activeFile.updateActiveFile();
+		this._activeFile.updateActiveFile();
 
-        this._disposable = Disposable.from(...subscriptions);
-    }
+		this._disposable = Disposable.from(...subscriptions);
+	}
 
-    dispose() {
-        this._disposable.dispose();
-    }
+	dispose() {
+		this._disposable.dispose();
+	}
 
-    private _onEvent() {
-        this._activeFile.updateActiveFile();
-    }
+	private _onEvent() {
+		this._activeFile.updateActiveFile();
+	}
 }
